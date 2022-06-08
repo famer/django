9d7aece9cf8c017d38dd6cf3be365a637aaf3998
Timur Tatarshaov <timurtatarshaov@Air-Timur.Dlink>
2022-06-08 20:20:56 +0300

    formatting with black

--- web/tests.py
+++ web/tests.py
@@ -4,7 +4,6 @@ from .models import Job, Tag, City
 
 # Create your tests here.
 class JobTestCase(TestCase):
-
     def setUp(self):
 
         # Create cities
@@ -16,42 +15,50 @@ class JobTestCase(TestCase):
         # Create tags
         express_tag = Tag.objects.create(title="Express.js")
         node_tag = Tag.objects.create(title="Node.js")
-        
+
         swift_tag = Tag.objects.create(title="Swift")
 
         python_tag = Tag.objects.create(title="Python")
         django_tag = Tag.objects.create(title="Django")
-        
+
         # Create jobs
         # Valid job
-        nodejs_job = Job.objects.create(title="Node.js developer",\
-                        company="OOO LTD",\
-                        description="Develop some greate Node.js application",\
-                        city=spb, contacts="Email: dj.famer@gmail.com, tg: famer",\
-                        net_salary_from=50000,\
-                        net_salary_to=12000,\
-                        moderated=True)
-                        
+        nodejs_job = Job.objects.create(
+            title="Node.js developer",
+            company="OOO LTD",
+            description="Develop some greate Node.js application",
+            city=spb,
+            contacts="Email: dj.famer@gmail.com, tg: famer",
+            net_salary_from=50000,
+            net_salary_to=12000,
+            moderated=True,
+        )
+
         nodejs_job.tags.add(express_tag)
         nodejs_job.tags.add(node_tag)
 
         # Invalid job, no description
-        swift_job = Job.objects.create(title="Swift developer",\
-                    city=moscow,\
-                    contacts="Email: dj.famer@gmail.com, tg: famer",\
-                    net_salary_from=70000,\
-                    net_salary_to=140000,\
-                    moderated=True)
+        swift_job = Job.objects.create(
+            title="Swift developer",
+            city=moscow,
+            contacts="Email: dj.famer@gmail.com, tg: famer",
+            net_salary_from=70000,
+            net_salary_to=140000,
+            moderated=True,
+        )
 
         swift_job.tags.add(swift_tag)
 
         # Not moderated
-        django_job = Job.objects.create(title="Django developer",\
-                    company="OOO LTD",\
-                    description="Develop some greate django application",\
-                    city=nn,\
-                    contacts="Email: dj.famer@gmail.com, tg: famer",\
-                    net_salary_from=60000, net_salary_to=120000)
+        django_job = Job.objects.create(
+            title="Django developer",
+            company="OOO LTD",
+            description="Develop some greate django application",
+            city=nn,
+            contacts="Email: dj.famer@gmail.com, tg: famer",
+            net_salary_from=60000,
+            net_salary_to=120000,
+        )
 
         django_job.tags.add(python_tag)
         django_job.tags.add(django_tag)
@@ -66,76 +73,92 @@ class JobTestCase(TestCase):
 
     def test_is_valid_job(self):
         nn = City.objects.get(title="Нижний Новгород")
-        j = Job.objects.get(title="Django developer",\
-                    company="OOO LTD",\
-                    description="Develop some greate django application",\
-                    city=nn, contacts="Email: dj.famer@gmail.com, tg: famer",\
-                    net_salary_from=60000, net_salary_to=120000)
+        j = Job.objects.get(
+            title="Django developer",
+            company="OOO LTD",
+            description="Develop some greate django application",
+            city=nn,
+            contacts="Email: dj.famer@gmail.com, tg: famer",
+            net_salary_from=60000,
+            net_salary_to=120000,
+        )
         self.assertTrue(j.is_valid_job())
 
     def test_is_invalid_salary(self):
         spb = City.objects.get(title="Санкт-Петербург")
-        j = Job.objects.get(title="Node.js developer",\
-                    company="OOO LTD",\
-                    description="Develop some greate Node.js application",\
-                    city=spb,\
-                    contacts="Email: dj.famer@gmail.com, tg: famer",\
-                    net_salary_from=50000, net_salary_to=12000)
+        j = Job.objects.get(
+            title="Node.js developer",
+            company="OOO LTD",
+            description="Develop some greate Node.js application",
+            city=spb,
+            contacts="Email: dj.famer@gmail.com, tg: famer",
+            net_salary_from=50000,
+            net_salary_to=12000,
+        )
         self.assertFalse(j.is_valid_job())
 
     def test_is_invalid_description(self):
         moscow = City.objects.get(title="Москва")
-        j = Job.objects.get(title="Swift developer",\
-                    city=moscow,\
-                    contacts="Email: dj.famer@gmail.com, tg: famer",\
-                    net_salary_from=70000, net_salary_to=140000)
+        j = Job.objects.get(
+            title="Swift developer",
+            city=moscow,
+            contacts="Email: dj.famer@gmail.com, tg: famer",
+            net_salary_from=70000,
+            net_salary_to=140000,
+        )
         self.assertFalse(j.is_valid_job())
 
     def test_index(self):
         c = Client()
-        response = c.get('/')
+        response = c.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['jobs'].count(), 2)
+        self.assertEqual(response.context["jobs"].count(), 2)
 
     def test_search(self):
         c = Client()
-        response = c.get('/jobs/?q=node')
+        response = c.get("/jobs/?q=node")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['jobs'].count(), 1)
+        self.assertEqual(response.context["jobs"].count(), 1)
 
     def test_search_onmoderated(self):
         c = Client()
-        response = c.get('/jobs/?q=django')
+        response = c.get("/jobs/?q=django")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['jobs'].count(), 0)
+        self.assertEqual(response.context["jobs"].count(), 0)
 
     def test_valid_job_page(self):
         city = City.objects.get(title="Санкт-Петербург")
-        j = Job.objects.get(title="Node.js developer",\
-                    company="OOO LTD",\
-                    description="Develop some greate Node.js application",\
-                    city=city,\
-                    contacts="Email: dj.famer@gmail.com, tg: famer",\
-                    net_salary_from=50000, net_salary_to=12000,\
-                    moderated=True)
+        j = Job.objects.get(
+            title="Node.js developer",
+            company="OOO LTD",
+            description="Develop some greate Node.js application",
+            city=city,
+            contacts="Email: dj.famer@gmail.com, tg: famer",
+            net_salary_from=50000,
+            net_salary_to=12000,
+            moderated=True,
+        )
 
         c = Client()
         response = c.get(f"/jobs/{j.id}")
         self.assertEqual(response.status_code, 200)
-    
+
     def test_unmodeerated_job_page(self):
-        city = City.objects.get(title='Нижний Новгород')
-        j = Job.objects.get(title="Django developer",\
-                    company="OOO LTD",\
-                    description="Develop some greate django application",\
-                    city=city,\
-                    contacts="Email: dj.famer@gmail.com, tg: famer",\
-                    net_salary_from=60000, net_salary_to=120000)
+        city = City.objects.get(title="Нижний Новгород")
+        j = Job.objects.get(
+            title="Django developer",
+            company="OOO LTD",
+            description="Develop some greate django application",
+            city=city,
+            contacts="Email: dj.famer@gmail.com, tg: famer",
+            net_salary_from=60000,
+            net_salary_to=120000,
+        )
         c = Client()
         response = c.get(f"/jobs/{j.id}")
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.context.get('jobs'), None)
-    
+        self.assertEqual(response.context.get("jobs"), None)
+
     def test_invalid_job_page(self):
         max_id = Job.objects.all().aggregate(Max("id"))["id__max"]
 
--- web/views.py
+++ web/views.py
@@ -10,55 +10,64 @@ from .forms import NewJobForm
 
 # Create your views here.
 def index(request):
-    jobs = Job.moderated_objects.order_by('-update_date')[:10]
-    return render(request, "web/index.html", {
-        "jobs": jobs
-    })
+    jobs = Job.moderated_objects.order_by("-update_date")[:10]
+    return render(request, "web/index.html", {"jobs": jobs})
+
 
 def jobs_list(request):
-    query = request.GET.get('q', '')
+    query = request.GET.get("q", "")
     jobs = Job.moderated_objects
     if query:
-        jobs = jobs.filter(Q(title__contains=query) | Q(description__contains=query) | Q(tags__title__contains=query))
-    
-    jobs = jobs.order_by('-update_date').distinct()[:10]
-    return render(request, "web/jobs.html", {
-        "jobs": jobs
-    })
+        jobs = jobs.filter(
+            Q(title__contains=query)
+            | Q(description__contains=query)
+            | Q(tags__title__contains=query)
+        )
+
+    jobs = jobs.order_by("-update_date").distinct()[:10]
+    return render(request, "web/jobs.html", {"jobs": jobs})
+
 
 def job(request, job_id):
     try:
         job = Job.objects.get(pk=job_id)
     except Job.DoesNotExist:
         raise Http404("404")
-    
+
     if not job.moderated:
         return render(request, "web/moderation.html", status=404)
 
-    return render(request, "web/job.html", {
-        "job": job
-    })
+    return render(request, "web/job.html", {"job": job})
+
 
 def job_add(request):
     if request.method == "POST":
         form = NewJobForm(request.POST)
         if form.is_valid():
-            title = form.cleaned_data['title']
-            company = form.cleaned_data['company']
-            description = form.cleaned_data['description']
-            contacts = form.cleaned_data['contacts']
-            city = form.cleaned_data['city']
-            net_salary_from = form.cleaned_data['net_salary_from']
-            net_salary_to = form.cleaned_data['net_salary_to']
+            title = form.cleaned_data["title"]
+            company = form.cleaned_data["company"]
+            description = form.cleaned_data["description"]
+            contacts = form.cleaned_data["contacts"]
+            city = form.cleaned_data["city"]
+            net_salary_from = form.cleaned_data["net_salary_from"]
+            net_salary_to = form.cleaned_data["net_salary_to"]
 
-            job = Job(title=title, description=description, city=city, company=company, contacts=contacts, net_salary_from=net_salary_from, net_salary_to=net_salary_to)
+            job = Job(
+                title=title,
+                description=description,
+                city=city,
+                company=company,
+                contacts=contacts,
+                net_salary_from=net_salary_from,
+                net_salary_to=net_salary_to,
+            )
             job.save()
 
-            if form.cleaned_data['tags']:
+            if form.cleaned_data["tags"]:
                 tags = []
-                for tag in form.cleaned_data['tags'].split(','):
+                for tag in form.cleaned_data["tags"].split(","):
                     tags.append(tag.strip())
-                
+
                 for tag in tags:
                     try:
                         db_tag = Tag.objects.get(title__iexact=tag)
@@ -67,16 +76,14 @@ def job_add(request):
                         db_tag.save()
 
                     job.tags.add(db_tag)
-            
-            return HttpResponseRedirect(reverse('job', args=(job.id,)))
-            
+
+            return HttpResponseRedirect(reverse("job", args=(job.id,)))
+
         else:
-            return render(request, "web/job_add.html", {
-                "form": form
-            })
+            return render(request, "web/job_add.html", {"form": form})
 
-    return render(request, "web/job_add.html", {
-        "cities": City.objects.all(),
-        "form": NewJobForm()
-    })
-
+    return render(
+        request,
+        "web/job_add.html",
+        {"cities": City.objects.all(), "form": NewJobForm()},
+    )
