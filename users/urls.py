eb2ca7ebfb5f3d858cfe9a8b55fa6a7c111ae4d0
Timur Tatarshaov <timurtatarshaov@MacBook-Pro-Timur.local>
2024-08-27 22:53:39 +0300

    replace login form

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from . import views
 from .views import SignUpView
+from django.contrib.auth.forms import AuthenticationForm
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup", views.signup_view, name="signup"),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
-    path("login", views.login_view, name="login"),
+    #path("login", views.login_view, name="login"),
+    path("login", AuthenticationForm.as_view(), name="login"),
     path("logout", views.logout_view, name="logout")
 ]
