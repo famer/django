31c9d3df1e90ef3d1cdf83b9023635e3cf081777
Timur Tatarshaov <timurtatarshaov@MacBook-Pro-Timur.local>
2024-08-30 11:43:51 +0300

    loggers for prod

--- itjobs/settings/prod.py
+++ itjobs/settings/prod.py
@@ -3,3 +3,29 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['itjobs.famer.keenetic.pro', 'gorbierd.ddns.net', 'localhost']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
