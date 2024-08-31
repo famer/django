64a01d83235ef01c8b770428b8db879ad4f2b597
Timur Tatarshaov <timurtatarshaov@MacBook-Pro-Timur.local>
2024-08-31 09:04:18 +0300

    change smtp port

--- itjobs/settings/prod.py
+++ itjobs/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['itjobs.famer.keenetic.pro', 'gorbierd.ddns.net', 'localhost']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
