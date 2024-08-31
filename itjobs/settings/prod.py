3893583c9206f44512c34cc27fb839f041d6a744
Timur Tatarshaov <timurtatarshaov@MacBook-Pro-Timur.local>
2024-08-31 09:07:28 +0300

    change smtp settings

--- itjobs/settings/prod.py
+++ itjobs/settings/prod.py
@@ -5,8 +5,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['itjobs.famer.keenetic.pro', 'gorbierd.ddns.net', 'localhost']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
