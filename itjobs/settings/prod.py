ef68dddb7d6a492ae91855c55d7490da5ee72534
Timur Tatarshaov <timurtatarshaov@MacBook-Pro-Timur.local>
2024-08-31 08:47:34 +0300

    prod email settings

--- itjobs/settings/prod.py
+++ itjobs/settings/prod.py
@@ -4,6 +4,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['itjobs.famer.keenetic.pro', 'gorbierd.ddns.net', 'localhost']
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
