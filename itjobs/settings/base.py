8b3ef1aff8c2f4b047faf933b65b6b6f8d74f466
Timur Tatarshaov <timurtatarshaov@MacBook-Pro-Timur.local>
2024-09-17 12:36:35 +0300

    change db name

--- itjobs/settings/base.py
+++ itjobs/settings/base.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'itjobs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME', 'postgres'),
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': 'db',
